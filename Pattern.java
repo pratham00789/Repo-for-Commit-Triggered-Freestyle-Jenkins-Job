ebbebb0523a171205ae5f8ec8446f8e89d419a52
pratham00789 <119481020+pratham00789@users.noreply.github.com>
2024-08-24 12:53:49 +0530

    Update Pattern.java
    added a print statement

--- Pattern.java
+++ Pattern.java
@@ -2,5 +2,6 @@ public class Pattern{
     public static void main(String[] args) {
         System.out.println("Hello, World! ");
         System.out.println("Hello, World! again ");
+        System.out.println("Hello, World! part 3 ");
     }
 }
