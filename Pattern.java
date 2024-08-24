f9f6fa7fec46e52fa5d67eaa241645362c88f09e
pratham00789 <119481020+pratham00789@users.noreply.github.com>
2024-08-24 12:10:57 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,5 +1,6 @@
 public class Pattern{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! ");
+        System.out.println("Hello, World! again ");
     }
 }
